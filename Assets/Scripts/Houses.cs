5bb20502972ef2d210f9765b474a17ad46222bdf
Ubuntu <ubuntu@ip-172-31-8-28.us-west-2.compute.internal>
2017-06-05 00:36:07 +0000

    Added houses

--- Assets/Scripts/Houses.cs
+++ Assets/Scripts/Houses.cs
@@ -4,12 +4,28 @@ using System.Collections.Generic;
 using System;
 
 public class Houses: MonoBehaviour {
-
+    
+    GameObject[] houses;
+    int housePointer = 0;
+    AssemblyLine assemblyLine;
     public void Start() {
+        assemblyLine = GameObject.Find("assemblyLine").GetComponent<AssemblyLine>();
+        houses = new GameObject[] {
+            GameObject.Find("house1-1"),
+            GameObject.Find("house1-2"),
+            GameObject.Find("house1-3"),
+            GameObject.Find("house1-4"),
+            GameObject.Find("house2-1"),
+            GameObject.Find("house2-2"),
+            GameObject.Find("house2-3"),
+            GameObject.Find("house2-4")
+        };
     }
     
-	public void Update()
-	{
-	}
-
+    public void Update() {
+        if (assemblyLine.profit % 3 == 1) {
+            houses[housePointer].GetComponent<SpriteRenderer>().enabled = true;
+            housePointer++;
+        }
+    } 
 }
